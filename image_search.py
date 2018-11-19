f69d8e9044271792585a235271e25288bf98e720
enki <b1014094@gmail.com>
2018-11-19 18:17:03 +0900

    least nine

--- image_search.py
+++ image_search.py
@@ -1,45 +1,55 @@
 import cv2
 import numpy as np
+import pickle
+import os
 from matplotlib import pyplot as plt
 
 result={}
 des_temp={}
+#des_data={}
+
+with open('des_file',mode='rb+') as file:
+	des_data=pickle.load(file)
 
 def im_read(img1,img2):
-	query=cv2.imread(img1+'.png')
-	comp=cv2.imread(img2+'.png')
+	query=cv2.imread(img1)
+	comp=cv2.imread('./dataset/'+img2)
+	cv2.imshow('query',query)
 	return query,comp
 
-def save_des(im1,des_1,im2,des_2):
-	des_temp[im1]=des_1
-	des_temp[im2]=des_2
-	np.save('des_file',des_temp)
+def save_des(imt,des_t):
+	des_temp[imt]=des_t
+	with open('des_file',mode='wb+') as file:
+		pickle.dump(des_temp,file)
 	print("saved!")
 
 
 def bfmatch_func(img1,img2):
 	query_img,train_img=im_read(img1,img2)
-
 	orb=cv2.ORB_create()
 	_,des_q=orb.detectAndCompute(query_img,None)
-	_,des_t=orb.detectAndCompute(train_img,None)
-	save_des(img1,des_q,img2,des_t)
+	if(img2 in des_data): #dataset中のdesを保存したい
+		des_t=des_data[img2]
+		print("aleady data")
+	else:
+		_,des_t=orb.detectAndCompute(train_img,None)
+		save_des(img2,des_t)
 
 	bf=cv2.BFMatcher(cv2.NORM_HAMMING,crossCheck=True)	
 	matches=bf.match(des_q,des_t)
 	dis= [m.distance for m in matches]
-	result[img2+'.png'] = sum(dis)/len(dis)
-	return result
+	result[img2] = sum(dis)/len(dis)
 	
 
 if __name__=='__main__':
-	print("main")
 	query=input('*Plese input query filename\n')
-	print(bfmatch_func('{}'.format(query),'test'))
-	print(np.load('des_file.npy'))
-
-	#img3=cv2.drawMatches(query_img,kp_q,train_img,kp_t,matches[:10],None,flags=2)
-	#plt.imshow(img3)
-	#plt.show()
-
-
+	files=os.listdir('./dataset')
+	for f in files:	
+		print(f)
+		bfmatch_func('{}'.format(query),f)
+	shortest_img=min(result,key=result.get)
+	result_img=cv2.imread('./dataset/'+shortest_img)
+	print("result: {}".format(shortest_img))
+	cv2.imshow(shortest_img,result_img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
