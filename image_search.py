92792bcc8d44b99553eb0cece46a9220e7a047fb
enki <b1014094@gmail.com>
2018-11-25 20:54:45 +0900

    ,pickle

--- image_search.py
+++ image_search.py
@@ -8,18 +8,18 @@ result={}
 des_temp={}
 #des_data={}
 
-with open('des_file',mode='rb+') as file:
+with open('des_file.pickle',mode='rb+') as file:
 	des_data=pickle.load(file)
 
 def im_read(img1,img2):
-	query=cv2.imread(img1)
-	comp=cv2.imread('./dataset/'+img2)
+	query=cv2.imread(img1,0)
+	comp=cv2.imread('./dataset/'+img2,0)
 	cv2.imshow('query',query)
 	return query,comp
 
 def save_des(imt,des_t):
 	des_temp[imt]=des_t
-	with open('des_file',mode='wb+') as file:
+	with open('des_file.pickle',mode='wb+') as file:
 		pickle.dump(des_temp,file)
 	print("saved!")
 
@@ -48,8 +48,10 @@ if __name__=='__main__':
 		print(f)
 		bfmatch_func('{}'.format(query),f)
 	shortest_img=min(result,key=result.get)
+	result=sorted(result.items(), key=lambda x:x[1])
 	result_img=cv2.imread('./dataset/'+shortest_img)
-	print("result: {}".format(shortest_img))
+	print("result for {}: {}".format(query,shortest_img))
+	print("shortest list:{}\n{}\n{}".format(result[0],result[1],result[2]))
 	cv2.imshow(shortest_img,result_img)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
