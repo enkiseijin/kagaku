1ee01878e2b573f7e22dc3e7513417f9257a6cd3
enki <b1014094@gmail.com>
2018-11-27 11:06:47 +0900

    owari

--- image_search.py
+++ image_search.py
@@ -11,11 +11,11 @@ des_data={}
 #with open('des_file.pickle',mode='rb+') as file:
 #	des_data=pickle.load(file)
 
-def im_read(img1,img2):
-	query=cv2.imread(img1,0)
+def im_read(img2):
+	#query=cv2.imread(img1,0)
 	comp=cv2.imread('./dataset/'+img2,0)
-	cv2.imshow('query',query)
-	return query,comp
+	#cv2.imshow('query',query)
+	return comp
 
 def save_des(imt,des_t):
 	des_temp[imt]=des_t
@@ -24,35 +24,42 @@ def save_des(imt,des_t):
 	print("saved!")
 
 
-def bfmatch_func(img1,img2):
-	query_img,train_img=im_read(img1,img2)
+def bfmatch_func(img1):
+	query_img=cv2.imread(img1,0)
+
 	orb=cv2.ORB_create()
 	kp_q,des_q=orb.detectAndCompute(query_img,None)
-	if(img2 in des_data): #dataset中のdesを保存したい
-		des_t=des_data[img2]
-		print("aleady data")
-	else:
-		kp_t,des_t=orb.detectAndCompute(train_img,None)
-		save_des(img2,des_t)
 
-	bf=cv2.BFMatcher(cv2.NORM_HAMMING,crossCheck=True)	
-	matches=bf.match(des_q,des_t)
-	matches = sorted(matches, key = lambda x:x.distance)
-	dis= [m.distance for m in matches]
-	result[img2] = sum(dis)/len(dis) #database画像ごとの距離平均
-	
+	mini=1000
+	files=os.listdir('./dataset')
+
+	for f in files:	
+		train_img=im_read(f)
+		kp_t,des_t=orb.detectAndCompute(train_img,None)
+
+
+		bf=cv2.BFMatcher(cv2.NORM_HAMMING,crossCheck=True)	
+		matches=bf.match(des_q,des_t)
+		dis= [m.distance for m in matches]
+		result[f] = sum(dis)/len(dis) #database画像ごとの距離平均
+		if(result[f]<mini):
+			mini=result[f]
+			mini_kp=kp_t
+			mini_img=train_img
+			mini_match=matches
+	return cv2.drawMatches(query_img,kp_q,mini_img,mini_kp,mini_match[:10],None,flags=2)
+
 
 if __name__=='__main__':
 	query=input('*Plese input query filename\n')
-	files=os.listdir('./dataset')
-	for f in files:	
-		print(f)
-		bfmatch_func('{}'.format(query),f)
+	match_kp_img=bfmatch_func(query)
+
 	shortest_img=min(result,key=result.get)
 	result=sorted(result.items(), key=lambda x:x[1])
 	result_img=cv2.imread('./dataset/'+shortest_img)
 	print("result for {}: {}".format(query,shortest_img))
 	print("shortest list:{}\n{}\n{}".format(result[0],result[1],result[2]))
-	cv2.imshow(shortest_img,result_img)
+	cv2.imshow('near_img',result_img)
+	cv2.imshow('key point matching',match_kp_img)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
